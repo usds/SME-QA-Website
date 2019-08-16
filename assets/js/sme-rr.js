d654e0feba6b934eac89a55feff904b2a9a9b13f
Matthew Dingee <matthew.j.dingee@omb.eop.gov>
2019-08-16 18:24:50 -0400

    Wires up the does not meet functionality.

--- assets/js/sme-rr.js
+++ assets/js/sme-rr.js
@@ -1,18 +1,23 @@
 // SME-QA: Resume Review - Prototype
 
 var $sme_rr = $('[data-object="sme-rr"]'),
-  meets_count = 0,
+  $sme_rr_select = $('.sme-rr-scoring-reason__select'),
   comps_count = 6,
   cleanUp = function () {
     // Cleans up the count, button states, reason, justification
+  },
+  revealSave = function (meets_count) {
+    var $final_save = $sme_rr.find('#sme-rr-final-save');
+
+    if ((meets_count + 1) == comps_count) {
+      $final_save.removeAttr('disabled');
+    }
   };
 
 $sme_rr.on('click', '[data-behavior]', function () {
   var $el = $(this),
     $object = $el.closest('[data-object="sme-rr"]'),
     behavior = $el.attr('data-behavior');
-
-  $el.blur(); // Removes focus
 
   // Each behavior attached to the element should be triggered
   $.each(behavior.split(' '), function (idx, action) {
@@ -23,9 +28,44 @@ $sme_rr.on('click', '[data-behavior]', function () {
 });
 
 $sme_rr.on('sme-rr.meets', function(event, opts) {
+  var $target = opts.object.find('#' + opts.el.attr('aria-controls')),
+    meets = opts.object.find('input:radio:checked').length;
+
   // Find how many are selected and set the count accordingly
+  revealSave(meets);
+
+  // Close the reason and justification if they are present
+  // TODO: Blank them out
+  $target.slideUp(function () {
+    $target.attr('aria-hidden', 'true');
+  });
 });
 
 $sme_rr.on('sme-rr.does-not-meet', function(event, opts) {
   // Reveal the reason and justification for that competency
+  var $target = opts.object.find('#' + opts.el.attr('aria-controls'));
+
+  $target.attr('aria-hidden', 'false');
+
+  $target.slideDown(function () {
+    $('html, body').animate({
+      scrollTop: opts.object.offset().top
+    });
+  });
+});
+
+$sme_rr_select.on('change', function () {
+  var $el = $(this),
+    $object = $el.closest('[data-object="sme-rr"]'),
+    reason_val = $el.val(),
+    $target = $object.find('#' + $el.attr('aria-controls')),
+    $input = $target.find('.sme-rr-scoring-reason__justification-text');
+
+  $target.attr('aria-hidden', false);
+
+  if (reason_val === 'other') {
+    $input.attr('aria-hidden', false);
+  } else {
+    $input.attr('aria-hidden', true);
+  }
 });
